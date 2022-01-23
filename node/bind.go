efb48208fbd9f94022bc35c51680d9aa481d6afc
Prad Nukala <pradn@sonr.io>
2022-01-23 17:18:40 -0500

    :zap: Update Motor node

---
+++ node/bind.go
@@ -0,0 +1,34 @@
+package motor
+
+// motor "github.com/sonr-io/sonr/cmd/motor/v1"
+
+// Start starts the host, node, and rpc service.
+func Start(reqBuf []byte) {
+	// ctx := context.Background()
+	// // Unmarshal request
+	// req := &motor.InitializeRequest{}
+	// if err := proto.Unmarshal(reqBuf, req); err != nil {
+	// 	golog.Warn("%s - Failed to unmarshal InitializeRequest. Using defaults...", err)
+	// }
+
+	// Start the app
+	// h, err := p2p.NewHost(ctx)
+	// if err != nil {
+	// 	golog.Fatal("%s - Failed to start host: %s", err)
+	// }
+}
+
+// Pause pauses the host, node, and rpc service.
+func Pause() {
+	// node.Pause()
+}
+
+// Resume resumes the host, node, and rpc service.
+func Resume() {
+	// node.Resume()
+}
+
+// Stop closes the host, node, and rpc service.
+func Stop() {
+	// node.Exit(0)
+}
