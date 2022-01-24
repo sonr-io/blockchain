f3b1fa2a05201cb183b2cce5db28a94a4a179d0a
Prad Nukala <pradn@sonr.io>
2022-01-24 08:54:47 -0500

    :art: Update Init Flag

--- cmd/sonrd/init_cli.go
+++ cmd/sonrd/init_cli.go
@@ -12,13 +12,13 @@ func init() {
 	// will be global for your application.
 
 	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.cli.yaml)")
-	RootCmd.AddCommand(commands.WalletCmd)
-	RootCmd.AddCommand(commands.ServeCmd)
-	RootCmd.AddCommand(commands.HighwayCmd)
-	RootCmd.AddCommand(commands.DeployCmd)
+	commands.RootCmd.AddCommand(commands.WalletCmd)
+	commands.RootCmd.AddCommand(commands.ServeCmd)
+	commands.RootCmd.AddCommand(commands.HighwayCmd)
+	commands.RootCmd.AddCommand(commands.DeployCmd)
 
-	RootCmd.AddCommand(commands.BindCmd)
+	commands.RootCmd.AddCommand(commands.BindCmd)
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	RootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	commands.RootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
