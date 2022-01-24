2d9af3702d8cd6201e9c01a7d1e82f8cb0809414
Prad Nukala <pradn@sonr.io>
2022-01-23 20:01:42 -0500

    :art: Update Goreleaser

--- cmd/sonrd/main.go
+++ cmd/sonrd/main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
 	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
 	"github.com/sonr-io/sonr/app"
-	"github.com/spf13/cobra"
 	cmd "github.com/tendermint/spm/cosmoscmd"
 )
 
@@ -19,21 +17,10 @@ func main() {
 		app.ModuleBasics,
 		app.New,
 		cmd.WithEnvPrefix("SONR_CHAIN"),
-		cmd.CustomizeStartCmd(start),
 		// this line is used by starport scaffolding # root/arguments
 
 	)
 	if err := svrcmd.Execute(rootCmd, app.DefaultNodeHome); err != nil {
 		os.Exit(1)
-	}
-}
-
-func start(cmd *cobra.Command) {
-	cmd.Aliases = append(cmd.Aliases, "sonr", "sonrd")
-	calledAs := cmd.CalledAs()
-	if calledAs == "sonr" {
-		fmt.Printf("[INFO] Starting %s\n", calledAs)
-		RootCmd.Execute()
-		select {}
 	}
 }
