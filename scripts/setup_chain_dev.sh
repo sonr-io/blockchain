87db3b0c2902e8b27d43728ea13ced8d248171e0
Nicholas Tindle <nicktindle@outlook.com>
2022-04-05 13:47:26 -0500

    Self Contained chain improvements

--- scripts/setup_chain_dev.sh
+++ scripts/setup_chain_dev.sh
@@ -4,13 +4,11 @@ sonrd init my-node --chain-id sonr
 export ALICE_TEXT=$(sonrd keys add --keyring-backend test alice --home /root/.sonr)
 export ALICE_ACCOUNT=$(echo $ALICE_TEXT| grep address | awk '{ print $2 }')
 echo "ALICE_TEXT:"; echo $ALICE_TEXT \n
-echo "ALICE_ACCOUNT:"; echo $(sonrd keys show -a alice --keyring-backend test)
 echo $ALICE_TEXT >> alice.txt
 
 # Setup a bob account
 export BOB_TEXT=$(sonrd keys add --keyring-backend test bob --home /root/.sonr| grep address | awk '{ print $2 }')
 echo "BOB_TEXT:"; echo $BOB_TEXT \n
-echo "BOB_ACCOUNT:"; echo $(sonrd keys show -a bob --keyring-backend test)
 echo $BOB_TEXT >> bob.txt
 # Setup the genesis accounts with $$
 sonrd add-genesis-account $(sonrd keys show -a alice --keyring-backend test) 1000000000000000stake,1000000000000snr
@@ -19,3 +17,6 @@ sonrd add-genesis-account $(sonrd keys show -a bob --keyring-backend test) 10000
 sonrd gentx alice 1000000000000000stake --keyring-backend test --chain-id sonr
 # collect the genesis transaction
 sonrd collect-gentxs
+# Echo the account addresses
+echo "ALICE_ACCOUNT:"; echo $(sonrd keys show -a alice --keyring-backend test)
+echo "BOB_ACCOUNT:"; echo $(sonrd keys show -a bob --keyring-backend test)
